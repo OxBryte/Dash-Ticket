aef92833bb03f2c4adeb5e8ac104d00d6713dd1d
OxBryte <silascyrax@gmail.com>
2025-12-01 12:20:43 +0100

    feat: updated seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs";
 const prisma = new PrismaClient();
 
 async function main() {
-  console.log("Start seeding with real data...")
+  console.log("Start seeding...")
 }
 
 main()
