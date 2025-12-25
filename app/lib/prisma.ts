0cb504140435500e239cedea204885ba3883bc02
OxBryte <silascyrax@gmail.com>
2025-12-25 22:21:32 +0100

    feat: updated prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -6,10 +6,6 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
-    transactionOptions: {
-      maxWait: 10000, // 10 seconds
-      timeout: 20000, // 20 seconds
-    },
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
